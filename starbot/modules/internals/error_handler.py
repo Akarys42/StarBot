f1c988831c8ad4a4b83833c152f53da54e2bd8a5
Matteo Bertucci <matteobertucci2004@gmail.com>
2022-01-30 12:20:12 +0100

    Error handler: add color to embed

--- starbot/modules/internals/error_handler.py
+++ starbot/modules/internals/error_handler.py
@@ -10,17 +10,20 @@ from starbot.exceptions import GuildNotConfiguredError
 logger = logging.getLogger(__name__)
 
 
-async def error_embed(ctx_or_inter: Context | ACI, message: str) -> None:
-    """Creates an error embed and send it to ctx_or_inter."""
-    await ctx_or_inter.send(embed=Embed(title="Error", description=message))
-
-
 class ErrorHandler(Cog):
-    """A general catch-all for errors occuring inside commands."""
+    """A general catch-all for errors occurring inside commands."""
 
     def __init__(self, bot: StarBot) -> None:
         """Initialize the ErrorHandler cog."""
         self.bot = bot
+
+    async def error_embed(self, ctx_or_inter: Context | ACI, message: str) -> None:
+        """Creates an error embed and send it to ctx_or_inter."""
+        config = await self.bot.get_config(ctx_or_inter)
+
+        await ctx_or_inter.send(
+            embed=Embed(title="Error", description=message, color=config.colors.danger)
+        )
 
     @Cog.listener()
     async def on_slash_command_error(self, inter: ACI, error: CommandError) -> None:
@@ -40,24 +43,24 @@ class ErrorHandler(Cog):
                     )
             # Disnake errors
             case errors.CommandOnCooldown():
-                await error_embed(inter, "This command is on cooldown.")
+                await self.error_embed(inter, "This command is on cooldown.")
             case errors.CheckFailure():
-                await error_embed(inter, "You do not have permission to use this command.")
+                await self.error_embed(inter, "You do not have permission to use this command.")
             case errors.CommandInvokeError():
                 logger.error(
                     f"Error while invoking command {inter.data.name} by {inter.author}: {error}",
                     exc_info=error.original,
                 )
-                await error_embed(
+                await self.error_embed(
                     inter,
                     "An error occurred while executing this command. Please let us know.",
                 )
             case errors.BadArgument():
-                await error_embed(inter, f"You have provided an invalid argument: {error}")
+                await self.error_embed(inter, f"You have provided an invalid argument: {error}")
             case errors.BadUnionArgument():
-                await error_embed(inter, f"You have provided an invalid argument: {error}")
+                await self.error_embed(inter, f"You have provided an invalid argument: {error}")
             case errors.UserInputError():
-                await error_embed(inter, f"Your input seems off: {error}")
+                await self.error_embed(inter, f"Your input seems off: {error}")
 
 
 def setup(bot: StarBot) -> None:
