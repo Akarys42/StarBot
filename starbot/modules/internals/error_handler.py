33c9ba27603d0361150d85956efabc2f1fe12f60
Matteo Bertucci <matteobertucci2004@gmail.com>
2022-02-02 14:00:44 +0000

    Error handler: make most messages ephemeral

--- starbot/modules/internals/error_handler.py
+++ starbot/modules/internals/error_handler.py
@@ -1,7 +1,7 @@
 import logging
 
 from disnake import Embed
-from disnake.ext.commands import Cog, CommandError, Context, errors
+from disnake.ext.commands import Cog, CommandError, errors
 
 from starbot.bot import StarBot
 from starbot.constants import ACI
@@ -17,15 +17,17 @@ class ErrorHandler(Cog):
         """Initialize the ErrorHandler cog."""
         self.bot = bot
 
-    async def error_embed(self, ctx_or_inter: Context | ACI, message: str) -> None:
+    async def error_embed(self, inter: ACI, message: str, ephemeral: bool = False) -> None:
         """Creates an error embed and send it to ctx_or_inter."""
         try:
-            config = await self.bot.get_config(ctx_or_inter)
+            config = await self.bot.get_config(inter)
             color = config.colors.danger
         except GuildNotConfiguredError:
             color = 0xED4245
 
-        await ctx_or_inter.send(embed=Embed(title="Error", description=message, color=color))
+        await inter.send(
+            embed=Embed(title="Error", description=message, color=color), ephemeral=ephemeral
+        )
 
     @Cog.listener()
     async def on_slash_command_error(self, inter: ACI, error: CommandError) -> None:
@@ -45,9 +47,11 @@ class ErrorHandler(Cog):
                     )
             # Disnake errors
             case errors.CommandOnCooldown():
-                await self.error_embed(inter, "This command is on cooldown.")
+                await self.error_embed(inter, "This command is on cooldown.", ephemeral=True)
             case errors.CheckFailure():
-                await self.error_embed(inter, "You do not have permission to use this command.")
+                await self.error_embed(
+                    inter, "You do not have permission to use this command.", ephemeral=True
+                )
             case errors.CommandInvokeError():
                 logger.error(
                     f"Error while invoking command {inter.data.name} by {inter.author}: {error}",
@@ -58,11 +62,15 @@ class ErrorHandler(Cog):
                     "An error occurred while executing this command. Please let us know.",
                 )
             case errors.BadArgument():
-                await self.error_embed(inter, f"You have provided an invalid argument: {error}")
+                await self.error_embed(
+                    inter, f"You have provided an invalid argument: {error}", ephemeral=True
+                )
             case errors.BadUnionArgument():
-                await self.error_embed(inter, f"You have provided an invalid argument: {error}")
+                await self.error_embed(
+                    inter, f"You have provided an invalid argument: {error}", ephemeral=True
+                )
             case errors.UserInputError():
-                await self.error_embed(inter, f"Your input seems off: {error}")
+                await self.error_embed(inter, f"Your input seems off: {error}", ephemeral=True)
 
 
 def setup(bot: StarBot) -> None:
