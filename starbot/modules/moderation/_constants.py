625d8abcc4f54321442de5ce07f13be65d57ad55
Matteo Bertucci <matteobertucci2004@gmail.com>
2022-03-22 07:44:15 +0000

    Infrac: check rank before infracting
    If the bot has a rank lower than the user infracted, the DM would still be sent but the infraction will fail to apply.
    This adds an additional check to prevent that.

--- starbot/modules/moderation/_constants.py
+++ starbot/modules/moderation/_constants.py
@@ -3,6 +3,7 @@ from starbot.models.infraction import InfractionTypes
 INFRACTIONS_WITH_DURATIONS = {InfractionTypes.MUTE}
 HIDDEN_INFRACTIONS = {InfractionTypes.NOTE}
 UNIQUE_INFRACTIONS = {InfractionTypes.MUTE, InfractionTypes.BAN}
+INFRACTION_REQUIRING_RANK = {InfractionTypes.MUTE, InfractionTypes.KICK, InfractionTypes.BAN}
 
 INFRACTION_NAME = {
     InfractionTypes.NOTE: "note",
